34d29e59da0a6a0d0678535aeacb93937d32b31f
Hudson <hudsonrodrigo06@gmail.com>
2021-01-12 11:09:32 -0300

    GRUPOS (Like Class)

--- PROG0001.cob
+++ PROG0001.cob
@@ -32,6 +32,23 @@
        78  C_TOTAL             VALUE 100.
       * -------------------*
 
+      *---- GRUPOS ----*
+       01  CLIENTE.
+           02  NOME            PIC A(20)   VALUE "HUDSON".
+           02  RG              PIC X(11)   VALUE "999999999".
+           02  IDADE           PIC 9(3)    VALUE 25.
+           02  TOTAL           PIC 999     VALUE 10.
+           02  CPF             PIC X(11)   VALUE "99999999999".
+           02  ENDERECO        PIC X(20)   VALUE "RUA X".
+           02  DTNASC.
+               03  DIA         PIC 99      VALUE 11.
+               03  MES         PIC 99      VALUE 01.
+               03  ANO         PIC 9999    VALUE 2021.
+
+       01  CARRO.
+           02  MARCA           PIC X(8)    VALUE "HYUNDAI".
+           02  COR             PIC X(10)   VALUE "VERMELHO".
+
 
 
       *================================================================*
@@ -40,7 +57,7 @@
 
       *     ADD 4 TO IDADE TOTAL OF CLIENTE
 
-           DISPLAY "Hello World".
+           DISPLAY "Cliente: " CLIENTE " Carro: " CARRO.
            STOP RUN.
 
        END PROGRAM VARIAVEIS-GRUPOS.
