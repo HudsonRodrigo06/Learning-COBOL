bbde4b70341dda39b42e54c0b8f92ac8b3efd096
Hudson <hudsonrodrigo06@gmail.com>
2021-01-12 13:56:27 -0300

    Receber dados do usuario (ACCEPT)

--- PROG0001.cob
+++ PROG0001.cob
@@ -20,53 +20,33 @@
        WORKING-STORAGE                                 SECTION.
       *----------------------------------------------------------------*
 
-      *---- VARIÁVEIS ----*
-       77  LETRAS              PIC A(20).
-       77  ALPHANUMERICOS      PIC X(20).
-       77  NUMEROS-INT         PIC 9(5).
-       77  NUMEROS-DOUBLE      PIC 9(5)V9(2).
-       77  NUMEROS-SINAL       PIC S9(5).
-      *-------------------*
-
-      *---- CONSTANTES ----*
-       78  C_TOTAL             VALUE 100.
-      * -------------------*
-
-      *---- GRUPOS ----*
-       01  CLIENTE.
-           02  NOME            PIC A(20)   VALUE "HUDSON".
-           02  RG              PIC X(11)   VALUE "999999999".
-           02  IDADE           PIC 9(3)    VALUE 25.
-           02  TOTAL           PIC 999     VALUE 10.
-           02  CPF             PIC X(11)   VALUE "99999999999".
-           02  ENDERECO        PIC X(20)   VALUE "RUA X".
-           02  DTNASC.
-               03  DIA         PIC 99      VALUE 11.
-               03  MES         PIC 99      VALUE 01.
-               03  ANO         PIC 9999    VALUE 2021.
-
-       01  CARRO.
-           02  MARCA           PIC X(8)    VALUE "HYUNDAI".
-           02  COR             PIC X(10)   VALUE "VERMELHO".
-           66  MARCA-COR RENAMES MARCA THRU COR.
-
-
-      *---- BOOLEAN RESULT IN GROUP -----*
-       01  GENERO              PIC X.
-           88  MASCULINO       VALUE "M".
-           88  FEMININO        VALUE "F".
-
-      *----------------------------------*
-
+       77  WK-MAIOR PIC 999 VALUE ZEROS.
+       77  WK-N1     PIC 999 VALUE ZEROS.
+       77  WK-N2     PIC 999 VALUE ZEROS.
+       77  WK-N3     PIC 999 VALUE ZEROS.
+       77  WK-QTDE  PIC 999 VALUE ZEROS.
 
       *================================================================*
        PROCEDURE                                               DIVISION.
       *================================================================*
 
-           ADD 1 1 TO IDADE TOTAL OF CLIENTE
-      *    ADD 1 GIVING IDADE --> Substitui o valor para 1
-           DISPLAY "Idade: " IDADE OF CLIENTE
-           DISPLAY " Total: " TOTAL OF CLIENTE
+           ACCEPT WK-N1.
+           ACCEPT WK-N2.
+           ACCEPT WK-N3.
+
+           IF WK-N1 > WK-MAIOR THEN
+               MOVE WK-N1 TO WK-MAIOR
+           END-IF.
+
+           IF WK-N2 > WK-MAIOR THEN
+               MOVE WK-N2 TO WK-MAIOR
+           END-IF.
+
+           IF WK-N3 > WK-MAIOR THEN
+               MOVE WK-N3 TO WK-MAIOR
+           END-IF.
+
+           DISPLAY "Maior: " WK-MAIOR.
 
            STOP RUN.
 
